3b5f25ca8682c59b1ebb9a43944da2c639f51987
coreygreenwald <coreygreen1108@gmail.com>
2023-03-15 12:56:27 -0400

    initial commit of beeswap-react public library

---
+++ index.tsx
@@ -0,0 +1,70 @@
+import React, {
+  useContext,
+  useEffect,
+  PropsWithChildren,
+  ReactElement,
+} from 'react';
+
+declare global {
+  interface Window {
+    renderBeeSwapAds: () => void;
+  }
+}
+
+export type RenderAdsProps = {
+  id?: string;
+  className?: string;
+}
+
+export type BeeSwapProps = {
+  beeId: string;
+  RenderBeeSwapAds: ({ id, className }: RenderAdsProps) => ReactElement;
+};
+
+export const BeeSwapContext = React.createContext<BeeSwapProps | null>(null);
+
+export const BeeSwapProvider = ({ children, beeId }: PropsWithChildren<{beeId: string}>) => {
+  async function fetchAndAttachScript() {
+    const script = document.createElement('script');
+    script.id = 'beeSwapScript';
+    script.setAttribute('data-beeswapid', beeId);
+    script.src = `https://hive.getbeeswap.com/embeds/${beeId}.js`;
+    script.async = true;
+    document.head.appendChild(script);
+  }
+
+  useEffect(() => {
+    fetchAndAttachScript();
+  }, []);
+
+  function RenderBeeSwapAds({ id, className }: RenderAdsProps) {
+    useEffect(() => {
+      window.renderBeeSwapAds();
+    });
+
+    return (
+      <div id={id} className={className} />
+    );
+  }
+
+  return (
+    <BeeSwapContext.Provider
+      value={{
+        beeId,
+        RenderBeeSwapAds,
+      }}
+    >
+      {children}
+    </BeeSwapContext.Provider>
+  );
+};
+
+function assertNever(): never {
+  throw Error('Cannot use useBeeSwap outside of BeeSwapContext');
+}
+
+export function useBeeSwap(): BeeSwapProps {
+  const context = useContext(BeeSwapContext);
+  if (!context) assertNever();
+  return context;
+}
